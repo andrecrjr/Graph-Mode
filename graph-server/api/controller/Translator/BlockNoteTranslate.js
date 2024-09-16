8286da89fe588306f4dbf3591118cbee4b4e4b3b
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-09-16 20:19:01 -0300

    - init translators

---
+++ graph-server/api/controller/Translator/BlockNoteTranslate.js
@@ -0,0 +1,68 @@
+class BlockNoteToNotionConverter {
+  convert(blockNoteBlocks) {
+    return blockNoteBlocks.map(block => this.convertBlock(block));
+  }
+
+  convertBlock(block) {
+    switch (block.type) {
+      case 'heading':
+        return this.convertHeading(block);
+      case 'paragraph':
+        return this.convertParagraph(block);
+      default:
+        throw new Error(`Unsupported block type: ${block.type}`);
+    }
+  }
+
+  convertHeading(block) {
+    // Notion heading levels go from 1 to 3, so we limit it here.
+    const headingLevel = Math.min(block.props.level || 1, 3);
+    
+    return {
+      object: 'block',
+      type: `heading_${headingLevel}`,
+      [`heading_${headingLevel}`]: {
+        rich_text: this.convertContent(block.content),
+        color: block.props.textColor,
+      }
+    };
+  }
+
+  convertParagraph(block) {
+    return {
+      object: 'block',
+      type: 'paragraph',
+      paragraph: {
+        rich_text: this.convertContent(block.content),
+        color: block.props.textColor,
+      }
+    };
+  }
+
+  convertContent(content) {
+    return content.map(item => this.convertText(item));
+  }
+
+  convertText(item) {
+    return {
+      type: 'text',
+      text: {
+        content: item.text
+      },
+      annotations: this.convertStyles(item.styles || {})
+    };
+  }
+
+  convertStyles(styles) {
+    return {
+      bold: styles.bold || false,
+      italic: styles.italic || false,
+      underline: styles.underline || false,
+      strikethrough: styles.strikethrough || false,
+      code: styles.code || false,
+      color: styles.textColor || 'default'
+    };
+  }
+}
+
+export default BlockNoteToNotionConverter
