e1c66831cecb4881cc36f45822c1f417645209dd
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-10-01 20:19:45 -0300

    - remove redirect add icon svg

--- front-end-graph/src/app/page.tsx
+++ front-end-graph/src/app/page.tsx
@@ -3,13 +3,8 @@ import { GeneralFooter } from "@/components/Footer";
 import { BGParticle } from "@/components/Home/BgParticle";
 
 import Landing from "@/components/Home/Landing";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const data = await auth();
-  if (!!data) {
-    redirect("/app");
-  }
   return (
     <div className="flex flex-col mt-5 h-full">
       <BGParticle />
