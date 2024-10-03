b81847ac0dae6ae8389b29e88f5b427383c726df
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-10-03 08:21:48 -0300

    - test scope again with callback url

--- front-end-graph/src/components/Buttons/index.tsx
+++ front-end-graph/src/components/Buttons/index.tsx
@@ -14,7 +14,11 @@ const AuthButton = () => {
         </Button>
       ) : (
         <Button
-          onClick={() => signIn("notion")}
+          onClick={() =>
+            signIn("notion", {
+              callbackUrl: "/app",
+            })
+          }
           className="w-fit text-white top-0 right-0 mr-4 font-semibold py-2 px-4 rounded-lg shadow mt-2 z-50"
         >
           Login with Notion
