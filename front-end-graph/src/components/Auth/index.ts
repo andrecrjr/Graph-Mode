7c1096986cbe41b9b6b61dfe5052dc9a2b582d69
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-10-15 20:08:08 -0300

    - hotfix server

--- front-end-graph/src/components/Auth/index.ts
+++ front-end-graph/src/components/Auth/index.ts
@@ -23,16 +23,16 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     },
     async session({ session, token, user }) {
       const userData = token.user as NotionProfile;
-      const resp = await fetch(process.env.SERVER_API + "/user", {
-        method: "POST",
-        body: JSON.stringify(userData),
-        headers: {
-          Authorization: `Bearer ${userData.tokens.access_token}`,
-          "Content-Type": "application/json",
-        },
-      });
-      const subscriptionData = await resp.json();
-      session.user = { ...userData, ...subscriptionData } as any;
+      // const resp = await fetch(process.env.SERVER_API + "/user", {
+      //   method: "POST",
+      //   body: JSON.stringify(userData),
+      //   headers: {
+      //     Authorization: `Bearer ${userData.tokens.access_token}`,
+      //     "Content-Type": "application/json",
+      //   },
+      // });
+      // const subscriptionData = await resp.json();
+      session.user = { ...userData } as any;
       return session;
     },
   },
