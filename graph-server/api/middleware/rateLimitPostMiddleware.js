45dc24702008698cac38ad97ef8c5b9ea13ef86f
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2025-01-07 22:29:22 -0300

    - delete key when is user inside redis

--- graph-server/api/middleware/rateLimitPostMiddleware.js
+++ graph-server/api/middleware/rateLimitPostMiddleware.js
@@ -1,7 +1,7 @@
 import {RedisController} from '../controller/RedisController/index.js';
 import logger from '../logs/index.js';
 
-const MAX_POSTS = 10; // Maximum posts allowed per day
+const MAX_POSTS = process.env.MAX_POSTS || 10; // Maximum fast notes allowed per day
 const DAILY_TIME = 86400;  // 86400 seconds = 24 hours
 
 export const rateLimitMiddleware = async (req, res, next) => {
@@ -10,10 +10,12 @@ export const rateLimitMiddleware = async (req, res, next) => {
         const userId = req.query?.user || 'anonymous'; // Replace with user ID from your auth logic
         const today = new Date().toISOString().split('T')[0]; // Format: YYYY-MM-DD
         const getUserAccount = await redis.getKey(`notion-${userId}`)
-        if(getUserAccount){
-            next()
-        }
         const rateLimitKey = `rate_limit:${userId}:${today}`;
+        if(getUserAccount){
+            await redis.deleteKey(rateLimitKey)
+            next()
+            return;
+        }
         
         const currentCount = await redis.getKey(rateLimitKey) || 0;
         if (currentCount >= MAX_POSTS) {
@@ -25,9 +27,10 @@ export const rateLimitMiddleware = async (req, res, next) => {
         }
 
         await redis.setKey(rateLimitKey, currentCount + 1, DAILY_TIME);
-        logger.info(`${req.query.user}`)
+        logger.info(`User count: ${req.query.user}-${currentCount+1}`)
 
-        next(); // Allow the request to proceed
+        next();
+        return;
     } catch (error) {
         console.error('Rate limit middleware error:', error);
         res.status(500).json({ error: true, message: 'Internal server error' });
