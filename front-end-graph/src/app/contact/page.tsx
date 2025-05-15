af097120fcac6872a456605e86ee597e763a5fe3
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2025-05-14 22:56:58 -0300

    fix: update iframe source URL and adjust height in ContactPage component

--- front-end-graph/src/app/contact/page.tsx
+++ front-end-graph/src/app/contact/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function ContactPage() {
     return (
-        <iframe src="https://acjr.notion.site/ebd/138b5e58148c80809666d2b33948b37e" width="100%" height="1000" />
+        <iframe src="https://acjr.notion.site/ebd/12db5e58148c80c19144ce5f22f3f392" width="100%" height="600" />
     )
 }
